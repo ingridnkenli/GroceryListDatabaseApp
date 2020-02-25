d517aded23c531c65c164a744a0a3dde565cc0b2
Ingrid Nkenlifack <i.nkenlifack@gmail.com>
2020-02-25 08:36:57 -0500

    Initial commit

---
+++ app/src/main/java/com/example/mysqlproject/DbStrings.java
@@ -0,0 +1,10 @@
+package com.example.mysqlproject;
+
+public class DbStrings {
+    static final String DATABASE_URL = "196.56.25.1:3306";
+    static final String DATABASE_NAME = "wampmysqld64.[nameoftable]";
+    static final String DATABASE_USERNAME = "root";
+    static final String DATABASE_PASSWORD = "my_pass";
+
+
+}
